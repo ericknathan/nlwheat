4672edbc795d0f39725f70869358c38fe5277f7f
ericknathan <erick.capito@hotmail.com>
2021-10-19 21:07:22 -0300

    :tada: first commit

---
+++ src/routes.ts
@@ -0,0 +1,32 @@
+import { Router } from 'express';
+import { AuthenticateUserController } from './controllers/AuthenticateUserController';
+import { CreateMessageController } from './controllers/CreateMessageController';
+import { GetLast3MessagesController } from './controllers/GetLast3MessagesController';
+import { ProfileUserController } from './controllers/ProfileUserController';
+import { ensureAuthenticated } from './middlewares/ensureAuthenticated';
+
+const router = Router();
+
+router.post('/authenticate', new AuthenticateUserController().handle);
+router.post(
+  '/messages',
+  ensureAuthenticated,
+  new CreateMessageController().handle
+);
+
+router.get('/messages/last3', new GetLast3MessagesController().handle);
+
+router.get('/profile', ensureAuthenticated, new ProfileUserController().handle);
+
+router.get('/github', (request, response) => {
+  response.redirect(
+    `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`
+  );
+});
+
+router.get('/signin/callback', (request, response) => {
+  const { code } = request.query;
+  response.json(code);
+});
+
+export { router };
