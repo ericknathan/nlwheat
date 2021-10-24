3bfe2f3347fc2039e7e8396f1d915a3f9a1f93b6
ericknathan <erick.capito@hotmail.com>
2021-10-23 22:29:34 -0300

    ✨ Add user email

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -11,34 +11,31 @@ interface IUserResponse {
   login: string;
   id: number;
   name: string;
+  email: string | null;
 }
 
 class AuthenticateUserService {
   async execute(code: string) {
     const url = 'https://github.com/login/oauth/access_token';
 
-    const { data: accessTokenResponse } =
-      await axios.post<IAccessTokenResponse>(url, null, {
-        params: {
-          client_id: process.env.GITHUB_CLIENT_ID,
-          client_secret: process.env.GITHUB_CLIENT_SECRET,
-          code
-        },
-        headers: {
-          Accept: 'application/json'
-        }
-      });
-
-    const response = await axios.get<IUserResponse>(
-      'https://api.github.com/user',
-      {
-        headers: {
-          authorization: `Bearer ${accessTokenResponse.access_token}`
-        }
+    const { data: accessTokenResponse } = await axios.post<IAccessTokenResponse>(url, null, {
+      params: {
+        client_id: process.env.GITHUB_CLIENT_ID,
+        client_secret: process.env.GITHUB_CLIENT_SECRET,
+        code
+      },
+      headers: {
+        Accept: 'application/json'
       }
-    );
+    });
 
-    const { login, id, avatar_url, name } = response.data;
+    const response = await axios.get<IUserResponse>('https://api.github.com/user', {
+      headers: {
+        authorization: `Bearer ${accessTokenResponse.access_token}`
+      }
+    });
+
+    const { login, id, avatar_url, name, email } = response.data;
 
     let user = await prismaClient.user.findFirst({
       where: {
@@ -50,9 +47,10 @@ class AuthenticateUserService {
       user = await prismaClient.user.create({
         data: {
           github_id: id,
+          email,
           login,
           avatar_url,
-          name
+          name,
         }
       });
     }
@@ -60,6 +58,7 @@ class AuthenticateUserService {
     const token = sign(
       {
         user: {
+          email: user.email,
           name: user.name,
           avatar_url: user.avatar_url,
           id: user.id
--- src/services/CreateMessageService.ts
+++ src/services/CreateMessageService.ts
@@ -1,5 +1,6 @@
 import prismaClient from '../prisma';
 import { io } from '../app';
+import axios from 'axios';
 
 class CreateMessageService {
   async execute(text: string, user_id: string) {
@@ -13,17 +14,29 @@ class CreateMessageService {
       }
     });
 
+    
     const infoWS = {
       text: message.text,
       user_id: message.user_id,
       created_at: message.created_at,
       user: {
         name: message.user.name,
-        avatar_url: message.user.avatar_url
+        avatar_url: message.user.avatar_url,
+        email: message.user.email
       }
     };
-
+    
     io.emit('new_message', infoWS);
+    
+    try {
+      await axios.post('http://localhost:5000/api/message', {
+        "message": text,
+        "username": message.user.name,
+        "email": message.user.email
+      });
+    } catch(err) {
+      console.log(err);
+    }
 
     return message;
   }
